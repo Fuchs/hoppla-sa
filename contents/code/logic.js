6c76edf179fe720fe97d6f1ba3933856029b9026
Christian (Fuchs) Loosli <git@fuchsnet.ch>
2016-12-19 13:19:24 +0100

    Add logic skeleton

---
+++ contents/code/logic.js
@@ -0,0 +1,16 @@
+function init()
+{
+    updateStatus();
+}
+
+function updateStatus()
+{
+    //TODO: Implement me
+}
+
+function icon()
+{
+    //TODO: Different icon depending on state?
+    //TODO: Actually create a nice lightbulb icon
+    return "im-jabber";
+}
