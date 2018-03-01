abc75efb8454b67f02232e6d751c1941dfbb035c
Christian (Fuchs) Loosli <git@fuchsnet.ch>
2018-03-01 20:28:06 +0100

    Use better parent for timer

--- package/contents/code/hue.js
+++ package/contents/code/hue.js
@@ -2194,5 +2194,5 @@ function dummyTranslator() {
 
 
 function Timer() {
-    return Qt.createQmlObject("import QtQuick 2.0; Timer {}", root);
+    return Qt.createQmlObject("import QtQuick 2.0; Timer {}", plasmoid);
 }
